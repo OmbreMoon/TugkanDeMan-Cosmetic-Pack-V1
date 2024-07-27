4fe31e3b9fc09e6b62de88fe741a878d292f3f9c
OmbreMoon <brycemcnair600@gmail.com>
2024-07-27 07:11:22 -0700

    Initial commit

---
+++ src/main/java/com/ombremoon/tdmcosmeticsone/Constants.java
@@ -0,0 +1,11 @@
+package com.ombremoon.sentinellib;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+    public static final String MOD_ID = "sentinellib";
+    public static final String MOD_NAME = "Sentinel Lib";
+    public static final String ABBR_NAME = "SL";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ src/main/java/com/ombremoon/tdmcosmeticsone/ItemInit.java
@@ -0,0 +1,2 @@
+package com.ombremoon.tdmcosmeticsone;public class ItemInit {
+}
